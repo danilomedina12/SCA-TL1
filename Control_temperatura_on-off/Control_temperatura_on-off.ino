419bc911bff2bb11528e0770eb8a62ce52c706cf
Danilo Medina <danilomedina666@gmail.com>
2025-05-08 22:13:02 -0300

    Se agrega encabezado (MostrarEncabezado). Se arregla ImprimirDatos para usar tabulaciones, se implica ApagarTodo() que utiliza un loop infinito

--- Control_temperatura_on-off/Control_temperatura_on-off.ino
+++ Control_temperatura_on-off/Control_temperatura_on-off.ino
@@ -1,38 +1,78 @@
 #include "control_temperatura.h" 
 #include "config.h"
-//#include <DallasTemperature.h>
 
 TemperaturaDatos DatosLeidos = {};  // Estructura de datos que leeremos
+unsigned long tiempoActual = 0;
+unsigned long tiempoUltimaMedicion = 0;
 
+void MostrarEncabezado();
 void ImprimirDatos();
+void ApagarTodo();
 
 void setup() {
   Serial.begin(9600);
   pinMode(STOPBUTTON_PIN, INPUT);
   TemperaturaInicializar();
-  // imprimir encabezado de datos
+  MostrarEncabezado();  // imprimir encabezado de datos
 }
 
 void loop() {
   unsigned long paradaDeEmergencia = digitalRead(STOPBUTTON_PIN);
+  tiempoActual = millis(); // actualiza el tiempo actual
 
   if (paradaDeEmergencia == HIGH) {
-    // llamar a otra función definida en este módulo
     TemperaturaParar(); 
+    ApagarTodo();// llamar a otra función definida en este módulo
+
   } else {
     // esto debe hacerse cada un tiempo determinado
-    TemperaturaMedir();
-    TemperaturaControlar(TEMP_OBJETIVO); // mide, controla, y gurda datos
-    TemperaturaLeer(&DatosLeidos);          // lee datos guardados
-    ImprimirDatos();
+    if(tiempoActual - tiempoUltimaMedicion >= TIEMPO_MUESTREO){
+      tiempoUltimaMedicion = tiempoActual;
+
+      TemperaturaMedir();
+      TemperaturaControlar(TEMP_OBJETIVO); // mide, controla, y gurda datos
+      TemperaturaLeer(&DatosLeidos);          // lee datos guardados
+      ImprimirDatos();
+    }
   }
+}
+
+void MostrarEncabezado() {
+  Serial.println("============================================");
+  Serial.println("CONTROL DE TEMPERATURA DE LA PAVA ELECTRICA");
+  Serial.println("============================================");
+  Serial.println("Datos: ");
+  Serial.print("   Temperatura referencia:  ");
+  Serial.print(TEMP_OBJETIVO);  // Usa macro de config.h
+  Serial.println(" ºC");
+  Serial.print("   Intervalo de histéresis: ±");
+  Serial.print(TEMP_HISTERESIS);
+  Serial.println(" ºC");
+  Serial.println("--------------------------------------------");
+  Serial.println(" ");
+  Serial.println("Tiempo\tTempAmb\tTempMed\tEstado");
+  Serial.println("[s]\t[°C]\t[°C]\t[Si/No]");
 }
 
 void ImprimirDatos() {
   // sólo imprimir dalos separados por tabulación...
-  // Queremos mostras: tiempo de muetreo / temp ambiente / temp medida / accion de control
-  Serial.print("Temperatura actual: ");
-  Serial.print(DatosLeidos.TemperaturaMedida);
-  Serial.print(" °C | Relé: ");
-  Serial.println(DatosLeidos.ActuadorEncendido ? "ENCENDIDO" : "APAGADO");
+  // Queremos mostrar: tiempo de muestreo / temp ambiente / temp medida / accion de control
+
+  unsigned long tiempoEnSegundos = DatosLeidos.TiempoMedicion / 1000;
+
+  Serial.print(tiempoEnSegundos);
+  Serial.print("\t");
+  Serial.print(DatosLeidos.TemperaturaAmbiente, 2);
+  Serial.print("\t");
+  Serial.print(DatosLeidos.TemperaturaMedida, 2);
+  Serial.print("\t");
+  Serial.println(DatosLeidos.ActuadorEncendido ? "SI" : "NO");
 }
+
+void ApagarTodo() {
+  Serial.println("SISTEMA APAGADO. REINICIAR PARA CONTINUAR.");
+  while (true) {
+    // loop infinito controlado para que el sistema quede apagado
+  }
+}
+
